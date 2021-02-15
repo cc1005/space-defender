c3a29061301ce50a10156c3d7261435c398f7885
cc1005 <conorkampbell@hotmail.com>
2021-02-15 17:56:08 +0000

    Added first waypoint and tested successfully. Now working on creating the full logic to allow random waves to be generated.

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -7,17 +7,12 @@ public class Laser : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        Destroy(this.gameObject);
     }
 }
