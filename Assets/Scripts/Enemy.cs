e5a4123e26e2badfe6bdbc3c48c0787d20cdd237
cc1005 <conorkampbell@hotmail.com>
2021-02-17 14:09:55 +0000

    Added lasers to the enemies, but currently when they fire them they destroy themselves and they also come out in bunches. Currently debugging

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,23 +6,54 @@ public class Enemy : MonoBehaviour
 {
 
     [SerializeField] float health = 100;
+    [SerializeField] float shotCounter;
+    [SerializeField] float minTimeBetweenShots = 0.2f;
+    [SerializeField] float maxTimeBetweenShots = 3f;
+    [SerializeField] GameObject enemyLaserPrefab;
+    [SerializeField] float enemyProjectileSpeed;
 
+    // Use this for initialization
+    private void Start()
+    {
+        shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CountDownAndShoot();
+    }
+
+    private void CountDownAndShoot()
+    {
+        shotCounter -= Time.deltaTime;
+        if (shotCounter <= 0f)
+        {
+            Fire();
+        }
+    }
+
+    private void Fire()
+    {
+        GameObject laser = Instantiate(
+                 enemyLaserPrefab,
+                 transform.position,
+                 Quaternion.identity) as GameObject;
+        laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, enemyProjectileSpeed);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+        ProcessHit(damageDealer);
+    }
+
+    private void ProcessHit(DamageDealer damageDealer)
+    {
         health -= damageDealer.GetDamage();
         if (health <= 0)
         {
             Destroy(gameObject);
         }
     }
-
-
 }
