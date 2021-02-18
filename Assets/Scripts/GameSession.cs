cd5d6955281921a4938690dba3eda67aa3e65a10
cc1005 <conorkampbell@hotmail.com>
2021-02-18 16:58:15 +0000

    Developing a singleton to carry score through to the final screen.

---
+++ Assets/Scripts/GameSession.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSession : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        SetUpScoreSingleton();
+    }
+
+    private void SetUpScoreSingleton()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        string sceneName = currentScene.name;
+        if (FindObjectsOfType(GetType()).Length > 1 || sceneName == "Start Menu")
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+            Debug.Log("I will print if the score singleton has loaded");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
